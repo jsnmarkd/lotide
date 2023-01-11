481ba998be9f89f817163f7595c25184d51c3fe6
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-11 18:31:26 -0500

    added another test to verify function

--- middle.js
+++ middle.js
@@ -46,3 +46,5 @@ console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])); // => [4, 5]
 console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // => [5, 6]
+
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]));
