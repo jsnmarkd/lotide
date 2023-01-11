a27a96fdc74107889641c857ddff46e857e440ef
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-10 21:02:49 -0500

    Had to readjust output

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
@@ -20,7 +20,7 @@ const eqArrays = function (a, b) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
--- eqArrays.js
+++ eqArrays.js
@@ -20,8 +20,13 @@ const eqArrays = function(a, b) {
   }
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
