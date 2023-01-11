275cb45b52bd7fd9f53f549c7a91a18af1785bcc
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-10 21:28:50 -0500

    Implement without.js assignment

---
+++ without.js
@@ -0,0 +1,44 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  const result = [...source] // copies array
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (result[i] === itemsToRemove[j]) {
+        result.splice(i, 1);
+      }
+    }
+  }
+  return result;
+};
+
+//TEST
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+console.log(words);
+console.log("without (lighthouse):", without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
