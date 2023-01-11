5c3d7263c671f5d1bf18daa05f3e5e4bfcb23cc2
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-11 08:23:36 -0500

    changed outputs to become more readable

--- without.js
+++ without.js
@@ -34,11 +34,10 @@ const without = function(source, itemsToRemove) {
 
 //TEST
 
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+console.log("without:", without([1, 2, 3], [1])); // => [2, 3]
+console.log("without:", without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
-console.log(words);
-console.log("without (lighthouse):", without(words, ["lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+console.log("array:", words); // print array
+console.log("without:", without(words, ["lighthouse"])); // no need to capture return value for this test case
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);// Make sure the original array was not altered by the without function
