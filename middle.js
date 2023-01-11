85819361b6548fdee2c3337191355da5b346e77f
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-11 18:03:45 -0500

    removed a for loop in the function that was not needed

--- middle.js
+++ middle.js
@@ -22,16 +22,14 @@ const assertArraysEqual = function(actual, expected) {
 
 const middle = function(array) {
   let middleArr = [];
-  for (i of array) {
-    if (array.length === 1 || array.length === 2) {
-      return middleArr;
-    } else if (array.length % 2 === 1) {
-      middleArr.push(array[Math.floor(array.length / 2)]);
-      return middleArr;
-    } else if (array.length % 2 === 0) {
-      middleArr.push(array[Math.floor(array.length / 2) - 1], array[Math.floor(array.length / 2)]);
-      return middleArr;
-    }
+  if (array.length === 1 || array.length === 2) {
+    return middleArr;
+  } else if (array.length % 2 === 1) {
+    middleArr.push(array[Math.floor(array.length / 2)]);
+    return middleArr;
+  } else if (array.length % 2 === 0) {
+    middleArr.push(array[Math.floor(array.length / 2) - 1], array[Math.floor(array.length / 2)]);
+    return middleArr;
   }
 };
 
