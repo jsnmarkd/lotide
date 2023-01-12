3ae7a2bd94851add981d547b7caff78ccf3ce6c2
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-11 19:12:24 -0500

    Implement letterPositions function to Lotide

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " ") {
+      if (!results[letter]) {
+        results[letter] = [i];
+      } else {
+        results[letter].push(i);
+      }
+    }
+  }
+  return results;
+};
+
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`⭕ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.log(letterPositions("hello"));
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
