4c8a32e3783ddad0b5415ae84eeadb8f1b9e7384
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-12 22:00:10 -0500

    Implement flatten.js in Lotide

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = (a, b) => {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = (array) => {
+  const flattened = [];
+  for (let i of array) {
+    if (Array.isArray(i)) {
+      for (let j of i) {
+        flattened.push(j);
+      }
+    } else {
+        flattened.push(i);
+    }
+  }
+  return flattened;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
