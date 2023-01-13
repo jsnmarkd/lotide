c043ba6eb7fd75bec8801db438cb98c4ea699ec4
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-12 21:15:52 -0500

    Implement findKey.js in Lotide

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  const keys = Object.keys(object);
+  for (let k of keys) {
+    if (callback(object[k])) {
+      return k;
+    }
+  }
+};
+
+const nestedObj = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+console.log(findKey(nestedObj, x => x.stars === 2));
+//EXPECTED => "noma"
+
+assertEqual(findKey(nestedObj, x => x.stars === 2), 'noma');
