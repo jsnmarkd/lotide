435041a6cc62c43c57e60ce4fb603d2964b5bca6
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-16 09:43:07 -0500

    Modularized assertEqual and head, also added a test folder and test files

--- head.js
+++ head.js
@@ -1,16 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(data) {
   return data[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head([]), "Hello");
+module.exports = head;
