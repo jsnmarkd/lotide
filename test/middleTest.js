1dfad746d2cef74403b68d21b76be2bc4edd5b4b
Jason Divinagracia <jmarkd04@gmail.com>
2023-01-16 14:37:17 -0500

    Implemented M&C for middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,18 +1,24 @@
 const middle = require('../middle');
 
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6, 7])); // => [4]
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9])); // => [5]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])); // => [4, 5]
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // => [5, 6]
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+describe("#middle", () => {
+  it(`returns [] for [1]`, () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it(`returns [] for [1, 2]`, () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it(`returns [2] for [1, 2, 3]`, () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it(`returns [5] for [1, 2, 3, 4, 5, 6, 7, 8, 9]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]);
+  });
+  it(`returns [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+  it(`returns [5, 6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]`, () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+  });
+});
